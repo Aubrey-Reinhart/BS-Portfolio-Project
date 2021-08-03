79d1960d35f0f29ec61eab0d1437fe1e760becac
Aubrey Reinhart <aubrey.reinhart@gmail.com>
2021-08-02 20:49:00 -0700

    Finished Navbar and Footer. Added carousel and bread imgs.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000, pause: "false" } ); //sets carousel rotation interval to 2 secs per image
+    $("#carouselButton").click(function(){ //runs code within to pause carousel on click
+        if ($("#carouselButton").children("i").hasClass("fa-pause")){ //when clicked jQuery function will check if there is a child node (i) with fa-pause
+            $(".carousel").carousel("pause"); //if it finds it, it knows the carousel is playing and will instead pause it
+            $("#carouselButton").children("i").removeClass("fa-pause"); //switch the font awesome icon from pause --
+            $("#carouselButton").children("i").addClass("fa-play"); //--to play
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play"); //switch the font awesome icon from pause --
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
