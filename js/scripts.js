9ec442d6033e52652253acce1da5ea06e31e29d7
Aubrey Reinhart <aubrey.reinhart@gmail.com>
2021-08-03 20:48:05 -0700

    Added content to index.html, aboutus.html, contactus.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(function() {
-    $(".carousel").carousel( { interval: 2000, pause: "false" } ); //sets carousel rotation interval to 2 secs per image
+    $(".carousel").carousel( { interval: 5000, pause: "false" } ); //sets carousel rotation interval to 2 secs per image
     $("#carouselButton").click(function(){ //runs code within to pause carousel on click
         if ($("#carouselButton").children("i").hasClass("fa-pause")){ //when clicked jQuery function will check if there is a child node (i) with fa-pause
             $(".carousel").carousel("pause"); //if it finds it, it knows the carousel is playing and will instead pause it
